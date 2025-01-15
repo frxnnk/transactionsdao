a928179b28622559ea203ff2141b709558159e98
frxnnk <frxnco@protonmail.com>
2025-01-15 17:18:10 -0300

    changes

--- api/transactions.js
+++ api/transactions.js
@@ -5,7 +5,44 @@ export default async function handler(req, res) {
 
     try {
         browser = await playwright.launchChromium({
-            headless: true
+            headless: true,
+            chromiumSandbox: false,
+            args: [
+                '--autoplay-policy=user-gesture-required',
+                '--disable-background-networking',
+                '--disable-background-timer-throttling',
+                '--disable-backgrounding-occluded-windows',
+                '--disable-breakpad',
+                '--disable-client-side-phishing-detection',
+                '--disable-component-update',
+                '--disable-default-apps',
+                '--disable-dev-shm-usage',
+                '--disable-domain-reliability',
+                '--disable-extensions',
+                '--disable-features=AudioServiceOutOfProcess',
+                '--disable-hang-monitor',
+                '--disable-ipc-flooding-protection',
+                '--disable-notifications',
+                '--disable-offer-store-unmasked-wallet-cards',
+                '--disable-popup-blocking',
+                '--disable-print-preview',
+                '--disable-prompt-on-repost',
+                '--disable-renderer-backgrounding',
+                '--disable-setuid-sandbox',
+                '--disable-speech-api',
+                '--disable-sync',
+                '--hide-scrollbars',
+                '--ignore-gpu-blacklist',
+                '--metrics-recording-only',
+                '--mute-audio',
+                '--no-default-browser-check',
+                '--no-first-run',
+                '--no-pings',
+                '--no-sandbox',
+                '--no-zygote',
+                '--single-process',
+                '--use-gl=swiftshader',
+            ]
         });
         
         const context = await browser.newContext({
